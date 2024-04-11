a6a247a48238e1f526e77b1f86e9658dbfe089b9
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-04-11 15:36:27 +0100

    add index

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('express')();
 const base = '/refreshwheeltransients';
+let status = 'Waiting';
 
 app.get(base, (req, res) => {
     res.send('Hello World! ENV is: ' + process.env.NODE_ENV);
@@ -8,6 +9,12 @@ app.get(base, (req, res) => {
 
 app.get(base + '/index', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
+});
+
+app.post(base + '/status', (req, res) => {
+    res.json({
+        status: status,
+    });
 });
 
 app.listen(3000, function () {
