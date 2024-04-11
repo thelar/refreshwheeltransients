cb21d12c2559749943528b1eb70e9547677d6959
Kevin Price-Ward <kevin.price-ward@4x4tyres.co.uk>
2024-04-11 20:16:47 +0100

    add index

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const app = require('express')();
+const bodyParser = require('body-parser');
 const base = '/refreshwheeltransients';
 let status = 'Waiting';
+let log = [];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get(base, (req, res) => {
     res.send('Hello World! ENV is: ' + process.env.NODE_ENV);
@@ -17,6 +21,26 @@ app.post(base + '/status', (req, res) => {
     });
 });
 
+app.post(base + '/command', (req, res) => {
+    let command = req.body.action;
+    console.log('command ' + command + ' received');
+    switch(req.body.action){
+        case 'start':
+            status = 'Started';
+            init();
+            break;
+        default:
+            break;
+    }
+    res.json({
+        status: status,
+    });
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+function init() {
+    log = [];
+}
